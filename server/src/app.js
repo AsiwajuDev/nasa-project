a4834b01897bf3d107b8ff4f3488b743e98e90be
asiwaju <olanrewajuranda@gmail.com>
2022-07-12 21:27:49 +0100

    Loads Planet from CSV

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
+// Import the router
+const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
+// configure cors
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.use(express.json());
+
+// Use the router for all requests
+app.use(planetsRouter);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,17 @@
 const http = require('http');
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const startServer = async () => {
+  await loadPlanetsData();
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
